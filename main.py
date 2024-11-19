41fa1288c23935085f93e885b3ed9e88965c3479
talish-gaurav <97028189+talish-gaurav@users.noreply.github.com>
2024-11-19 19:18:07 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+
+n = int(input("Enter your number here"))
+p = int(input("Enter the power you would like your number up to"))
+prod = 1
+
+for c in range(p):
+
+    prod = prod*n
+
+print("Power is :", prod)
